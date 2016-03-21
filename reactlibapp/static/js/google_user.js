a998993a9d7a056b47f588f2dc7065635d526c62
andela-sjames <samuel.james@andela.com>
2016-03-21 01:41:13 +0100

    get basic info set

---
+++ reactlibapp/static/js/google_user.js
@@ -0,0 +1,29 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+
+
+    $.ajax({
+            type: "GET",
+            url: "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + id_token,
+            success: function(data) {
+                console.log(data)
+            },
+
+            error: function(error) {
+                    console.log(error.responseText)
+                },
+        });
+
+
+
+
+
+    console.log('id_token: ' + id_token);
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
+
+'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=XYZ123'
