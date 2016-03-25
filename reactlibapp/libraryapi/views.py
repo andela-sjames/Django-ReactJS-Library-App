b29a57ce6041c06ba7092f073075d6ccbf684e15
andela-sjames <samuel.james@andela.com>
2016-03-25 02:07:34 +0100

    add api view for category

--- reactlibapp/libraryapi/views.py
+++ reactlibapp/libraryapi/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework import filters
+
+from libraryapp.models import Category
+from libraryapi.serializers import CategorySerializer
+from libraryapi.setpagination import LimitOffsetpage
+
+
+class CategoryList(ListAPIView):
+     """List all Categories."""
+
+    model = Category
+    serializer_class = CategorySerializer
+    pagination_class = LimitOffsetpage
+    filter_fields = ('name',)
+    queryset = Category.objects.all()
