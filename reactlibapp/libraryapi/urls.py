68e97f2f8662a4c18837414740dc0cb15fc69154
andela-sjames <samuel.james@andela.com>
2016-03-25 12:55:50 +0100

    add api endpoints for history, booklist and interest.

--- reactlibapp/libraryapi/urls.py
+++ reactlibapp/libraryapi/urls.py
@@ -4,8 +4,20 @@ from libraryapi import views, usertoken
 
 
 urlpatterns = [
-    url(r'^categories/$', views.CategoryList.as_view(),
+    url(r'^categories/$', views.CategoryListView.as_view(),
         name='apicategory'),
+
+    url(r'^authors/$', views.AuthorListView.as_view(),
+        name='apiauthors'),
+
+    url(r'^books/$', views.BookListView.as_view(),
+        name='apibooks'),
+
+    url(r'^history/$', views.HistoryListView.as_view(),
+        name='apihistory'),
+
+    url(r'^interest/$', views.InterestListView.as_view(),
+        name='api_interest'),
 ]
 
 
--- reactlibapp/libraryapi/views.py
+++ reactlibapp/libraryapi/views.py
@@ -1,16 +1,17 @@
 from django.http import Http404
 
 from rest_framework import status
+from rest_framework import filters
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView, ListAPIView
-from rest_framework import filters
 
-from libraryapp.models import Category
-from libraryapi.serializers import CategorySerializer
 from libraryapi.setpagination import LimitOffsetpage
+from libraryapp.models import Category, Author, Book, History, Interest
+from libraryapi.serializers import CategorySerializer, AuthorSerializer, BookSerializer,\
+    HistorySerializer, InterestSerializer
 
 
-class CategoryList(ListAPIView):
+class CategoryListView(ListAPIView):
      """List all Categories."""
 
     model = Category
@@ -18,3 +19,62 @@ class CategoryList(ListAPIView):
     pagination_class = LimitOffsetpage
     filter_fields = ('name',)
     queryset = Category.objects.all()
+
+
+class AuthorListView(ListAPIView):
+    """List all Authors."""
+
+    model = Author
+    serializer_class = AuthorSerializer
+    pagination_class = LimitOffsetpage
+    filter_fields = ('name',)
+    queryset = Author.objects.all()
+
+
+class BookListView(ListAPIView):
+    """List all books."""
+
+    model = Book
+    serializer_class = BookSerializer
+    pagination_class = LimitOffsetpage
+    # filter_fields = ()
+    queryset = Book.objects.all()
+
+
+class HistoryListView(ListAPIView):
+    """List all history belonging to a particular user."""
+
+    model = History
+    serializer_class = HistorySerializer
+    pagination_class = LimitOffsetpage
+    # filter_fields = ()
+
+    def get_queryset(self):
+        """
+        This view should return a list for
+        the currently authenticated user's history.
+        """
+        quser = self.request.user.id
+        queryset = History.objects.filter(user=quser)
+
+        return queryset
+
+
+class InterestListView(ListAPIView):
+    """List all users interest in a book."""
+
+    model = Interest
+    serializer_class = InterestSerializer
+    pagination_class = LimitOffsetpage
+    # filter_fields = ()
+
+    def get_queryset(self):
+        """
+        This view should return a list for
+        the currently authenticated user's interest.
+        """
+        quser = self.request.user.id
+        queryset = Interest.objects.filter(user=quser)
+
+        return queryset
+
