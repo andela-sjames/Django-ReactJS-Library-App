b3b50a99f210f7ef68a8cb5d4bc65b52ca7c967f
andela-sjames <samuel.james@andela.com>
2016-03-30 01:46:29 +0100

    add typedjs for animated typewriting.

---
+++ reactlibapp/static/js/transition.js
@@ -0,0 +1,13 @@
+(function(){
+
+     $("#quotesDisplay").typed({
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 0,
+            loop: true,
+            showCursor: true,
+            contentType: 'html',
+            typeSpeed: 90,
+            backSpeed: 30,
+        });
+
+})();
