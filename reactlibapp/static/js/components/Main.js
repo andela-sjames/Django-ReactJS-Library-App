4321f7d2e8e9d8dbd129120bd49e3adbea2e5d36
andela-sjames <samuel.james@andela.com>
2016-04-14 23:45:02 +0100

    fetch google user detail.

--- reactlibapp/static/js/components/Main.js
+++ reactlibapp/static/js/components/Main.js
@@ -1,15 +1,10 @@
 var React = require('react');
 require('../../css/styles.css')
-
-var styles = {
-
-    header: {
-        color: '#4A90E2',
-      },
-}
+var axios = require('axios')
 
 var Header = React.createClass({
     render: function(){
+        console.log(this.props);
         return (
             <div id="head" className="ui block header">
                 <div className="ui grid">
@@ -17,8 +12,9 @@ var Header = React.createClass({
                         <div className="left floated column">
                             <h1 id="title">CuriousMind</h1>
                         </div>
-                        <div className="right floated column">
-                            <h1 id="title">Logged in as...</h1>
+                        <div className="right floated column cont">
+                            <div className="item"><span id="title">Logged in as {this.props.username}</span></div>
+                            <div className="item"><img src={this.props.image} className="ui mini circular image"/></div>
                         </div>
                     </div>
                 </div>
@@ -43,10 +39,38 @@ var Footer = React.createClass({
 
 var Main = React.createClass({
 
+    getInitialState: function() {
+        return {
+            username: '',
+            image_url: ''
+        };
+    },
+
+    getUserDetail: function() {
+        return axios.get('/api/appuser/');
+    },
+
+    getResult: function(response) {
+        this.setState({
+            username: response.data.app_user.username,
+            image_url: response.data.appuser_picture,
+        });
+
+    },
+
+    componentDidMount: function() {
+        return this.getUserDetail()
+            .then(this.getResult)
+              .catch(function (response) {
+                console.log(response);
+            });
+    },
+
+
     render: function() {
         return (
             <div className='main-container'>
-                <Header />
+                <Header username={this.state.username} image={this.state.image_url}/>
                 {this.props.children}
                 <Footer />
             </div>
