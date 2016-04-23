09f080ba52a720c7c30d685da95028fd79cef318
andela-sjames <samuel.james@andela.com>
2016-04-23 23:44:57 +0100

    add methods for books components.

--- reactlibapp/static/js/components/Books.js
+++ reactlibapp/static/js/components/Books.js
@@ -3,6 +3,31 @@ require('../../css/styles.css')
 
 var Books = React.createClass({
 
+    getInitialState: function() {
+        return {
+            username: '',
+            image_url: ''
+        };
+    },
+
+    getBookDetail: function() {
+        return axios.get('/api/books/');
+    },
+
+    getSetResult: function(response) {
+        this.setState({
+
+        });
+    }
+
+    componentDidMount: function() {
+        this.getBookDetail()
+            .then(this.getSetResult)
+              .catch(function (response) {
+                console.log(response);
+        });
+    },
+
     render: function() {
         return (
             <div>
