e7399388db4229138c3bf7aaa677a7f96f7d82ef
Princess-Jewel Essien <princess-jewel.essien@andela.com>
2017-08-10 18:16:14 +0100

    chore(react-setup): set up test development environment
    - install and configure various testing libraries
    - add a few sample tests

--- reactlibapp/client/src/reducers/persistReducer.ts
+++ reactlibapp/client/src/reducers/persistReducer.ts
@@ -1,11 +1,9 @@
 import { persist } from '../actions/actionTypes';
 import { PersistAction } from '../actions/actionTypes.d';
 
-/**
- * Loads state persisted with redux-persist.
- * TODO: Distribute to individual reducers (to preserve app structure)
- */
-export default function persistReducer(state:object = {}, action:PersistAction) {
+// Loads state persisted with redux-persist.
+// TODO: Distribute to individual reducers (to preserve app structure)
+export default function persistReducer(state:object = {}, action:PersistAction = {}) {
   switch (action.type) {
     case persist.RESTORE_STORE:
       return { ...state, ...action.payload };
