35578835ab4b84014753a2487479237f3179bbe9
BolajiOlajide <bolaji.olajide@andela.com>
2017-11-16 00:58:35 +0100

    automate virtual environment for BEND

--- reactlibapp/scripts/launch.sh
+++ reactlibapp/scripts/launch.sh
@@ -6,7 +6,7 @@ set -e
 set -o pipefail
 
 # print each step of your code to the terminal
-set -x
+# set -x
 
 function setup_client() {
   # navigate into the client folder to run webpack
@@ -28,9 +28,27 @@ function setup_client() {
   cd ..
 }
 
+function auto_venv() {
+  echo 'Automating virtual environment activation'
+  # check if a virtualenvironment has already been created before
+  if ! [ -d "venv" ]; then
+    # create a virtualenvironment with Python 3
+    virtualenv --python=python3 venv
+  fi
+
+  source venv/bin/activate
+  echo 'Yo!'
+}
+
 function setup_server() {
   # run any pending migrations
   cd ..
+  echo 'Do you need me to automate virtual environment?'
+  echo 'Enter y for yes, n for no'
+  read response
+  if [ $response == "y" ]; then
+    auto_venv
+  fi
   pip3 install -r requirements.txt
   python manage.py makemigrations
   python manage.py migrate
@@ -47,4 +65,5 @@ esac
 # if in production, setup_client will exit once building is done
 # this means you need to wait for webpack's initial output before attempting
 # to launch the app in a browser
-setup_server && setup_client & cd .. && python manage.py runserver 0.0.0.0:8000
+# setup_server && setup_client & cd .. && python manage.py runserver 0.0.0.0:8000
+setup_server
