bd67a83ae63e582a7c062795539c18d2c0e14a9f
Bolaji Olajide <bolaji.olajide@andela.com>
2017-11-20 12:40:17 +0100

    update docker configuration (#44)

--- reactlibapp/scripts/launch.sh
+++ reactlibapp/scripts/launch.sh
@@ -64,4 +64,4 @@ esac
 # if in production, setup_client will exit once building is done
 # this means you need to wait for webpack's initial output before attempting
 # to launch the app in a browser
-setup_server && setup_client & cd .. && python manage.py runserver 0.0.0.0:8000
+setup_server && setup_client & python manage.py runserver 0.0.0.0:8000
